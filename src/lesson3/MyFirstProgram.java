f0aeb83674b66524f8a6055ff27ad0d652694601
Radu Nastas <nastasradudev@gmail.com>
2025-05-07 13:45:37 +0200

    Homework lesson 3

---
+++ src/lesson3/MyFirstProgram.java
@@ -0,0 +1,20 @@
+package lesson3;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        byte numberOfRanches = 4;
+        short numberOfHorses = 120;
+        int numberOfHorseshoes = 480;
+        long numberOfBees = 123456789;
+
+        float depositAmountInCoins = 3456.8587f;
+        double depositAmountInBanknotes = 345678.919866;
+
+        char thirdLetterOfAlphabet = 'C';
+        boolean lastLetterOfAlphabetIsA = false;
+        boolean lastLetterOfAlphabetIsZ = true;
+    }
+
+}
