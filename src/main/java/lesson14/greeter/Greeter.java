af1a760b8acd24727d023a587e131e919d70a248
RaduNastas <nastasradudev@gmail.com>
2025-05-29 13:30:47 +0200

    homework for lesson 14 & 15

---
+++ src/main/java/lesson14/greeter/Greeter.java
@@ -0,0 +1,12 @@
+package lesson14.greeter;
+
+public class Greeter {
+
+    public void greet(String name, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(name);
+
+        }
+    }
+
+}
