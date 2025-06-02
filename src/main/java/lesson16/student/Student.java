d42c6f7faf7d9244665cd298e90dbac8373e0ba5
RaduNastas <nastasradudev@gmail.com>
2025-06-02 18:18:09 +0200

    Homework lesson 16 Student and Group

---
+++ src/main/java/lesson16/student/Student.java
@@ -0,0 +1,26 @@
+package lesson16.student;
+
+public class Student {
+
+    private String name;
+    private String groupName;
+
+    public Student(String name, String groupName) {
+        this.name = name;
+        this.groupName = groupName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', groupName='" + groupName + "'}";
+    }
+
+}
